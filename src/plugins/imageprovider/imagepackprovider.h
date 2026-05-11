b888699bfaac1c4521954babe4361e15a33ce02a
zanyxdev <23155424+ZanyXDev@users.noreply.github.com>
2026-05-11 23:49:29 +0300

    rewrite ImagePackProvider::requestPixmap. add checkers and readers file

--- src/plugins/imageprovider/imagepackprovider.h
+++ src/plugins/imageprovider/imagepackprovider.h
@@ -10,7 +10,10 @@ class ImagePackProvider : public QQuickImageProvider
 public:
     ImagePackProvider();
     QPixmap requestPixmap(const QString &id, QSize *size, const QSize &requestedSize) override;
+    const QStringList& packNames() const{ return m_packs; };  // Константная ссылка
+
 private:
-    QStringList m_imagesPackList;
+    QStringList m_images;
+    QStringList m_packs;
     void findImagesInPacks();
 };
